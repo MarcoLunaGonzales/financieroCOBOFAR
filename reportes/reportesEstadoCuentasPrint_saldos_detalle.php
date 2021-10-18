38b6fd0797171d5081e2d97bbcd44b731962c9d1
= <=>
2021-10-17 23:10:27 -0400

    reporte saldos detallado

--- reportes/reportesEstadoCuentasPrint_saldos_detalle.php
+++ reportes/reportesEstadoCuentasPrint_saldos_detalle.php
@@ -1,55 +1,52 @@
 <?php
 
+function generarHTMLFacCliente($cuentai,$NombreGestion,$sqlFechaEstadoCuenta,$StringUnidades,$cod_cuentaauxX,$unidadCostoArray,$areaCostoArray,$desde,$hasta,$monto_periodo,$array_periodo){
+    require_once __DIR__.'/../conexion.php';            
+    require_once __DIR__.'/../functionsGeneral.php';
+    $dbh = new Conexion();
 
+$saldo_X=0;
+$sql="SELECT e.fecha,e.monto
+    FROM estados_cuenta e,comprobantes_detalle d, comprobantes cc, cuentas_auxiliares ca  where e.cod_comprobantedetalle=d.codigo and cc.codigo=d.cod_comprobante and e.cod_cuentaaux=ca.codigo and cc.cod_estadocomprobante<>2 and d.cod_cuenta in ($cuentai) and e.cod_comprobantedetalleorigen=0 and cc.cod_gestion= '$NombreGestion' $sqlFechaEstadoCuenta and cc.cod_unidadorganizacional in ($StringUnidades) and e.cod_cuentaaux in ($cod_cuentaauxX) and d.cod_unidadorganizacional in ($unidadCostoArray) and d.cod_area in ($areaCostoArray) order by e.fecha"; //ca.nombre, 
+ // echo $sql;
+$stmtUO = $dbh->prepare($sql);
+$stmtUO->execute();
 
-$periodo=0;
-foreach ($array_periodo as $periodo) {
-    
-    $fecha_hasta=date('Y-m-d',strtotime($desde.'+'.$periodo.' day'));
-
-    $sqlFechaEstadoCuentadet="and e.fecha BETWEEN '$desde 00:00:00' and '$fecha_hasta 23:59:59'";
-
-    $sql="SELECT e.fecha,e.cod_cuentaaux,sum(e.monto)as monto_ec,ca.nombre,(SELECT c.tipo from configuracion_estadocuentas c where c.cod_plancuenta=d.cod_cuenta)as tipoDebeHaber
-        FROM estados_cuenta e,comprobantes_detalle d, comprobantes cc, cuentas_auxiliares ca  where e.cod_comprobantedetalle=d.codigo and cc.codigo=d.cod_comprobante and e.cod_cuentaaux=ca.codigo and cc.cod_estadocomprobante<>2 and d.cod_cuenta in ($cuentai) and e.cod_comprobantedetalleorigen=0 and cc.cod_gestion= '$NombreGestion' $sqlFechaEstadoCuentadet and cc.cod_unidadorganizacional in ($StringUnidades) $proveedoresStringAux and d.cod_unidadorganizacional in ($unidadCostoArray) and d.cod_area in ($areaCostoArray) GROUP BY e.cod_cuentaaux  order by e.fecha"; //ca.nombre, 
-    // echo $sql;
-    $stmtUO = $dbh->prepare($sql);
-    $stmtUO->execute();
-    $index=1;
-    while ($row = $stmtUO->fetch()) {
-        $monto_ecX=$row['monto_ec'];
-        $cod_cuentaauxX=$row['cod_cuentaaux'];
-        $nombreX=$row['nombre'];
-        $tipoDebeHaberX=$row['tipoDebeHaber'];   
-        //PAGADO
-        $sql="SELECT sum(e.monto)as monto_ec
-        FROM estados_cuenta e,comprobantes_detalle d, comprobantes cc, cuentas_auxiliares ca  where e.cod_comprobantedetalle=d.codigo and cc.codigo=d.cod_comprobante and e.cod_cuentaaux=ca.codigo and cc.cod_estadocomprobante<>2 and d.cod_cuenta in ($cuentai) and e.cod_comprobantedetalleorigen>0 and cc.cod_gestion= '$NombreGestion' $sqlFechaEstadoCuenta and cc.cod_unidadorganizacional in ($StringUnidades) and e.cod_cuentaaux in ($cod_cuentaauxX) and d.cod_unidadorganizacional in ($unidadCostoArray) and d.cod_area in ($areaCostoArray) GROUP BY e.cod_cuentaaux  order by e.fecha";
-        $stmt_d = $dbh->prepare($sql);
-        $stmt_d->execute();
-        $monto_ecD=0;
-        while ($row_d = $stmt_d->fetch()) {
-            $monto_ecD=$row_d['monto_ec'];
+while ($row = $stmtUO->fetch()) {
+    $fechaDet=$row['fecha'];
+    $monto_ecX=$row['monto'];
+    //PAGADO
+    $sql="SELECT e.monto
+    FROM estados_cuenta e,comprobantes_detalle d, comprobantes cc, cuentas_auxiliares ca  where e.cod_comprobantedetalle=d.codigo and cc.codigo=d.cod_comprobante and e.cod_cuentaaux=ca.codigo and cc.cod_estadocomprobante<>2 and d.cod_cuenta in ($cuentai) and e.cod_comprobantedetalleorigen>0 and cc.cod_gestion= '$NombreGestion' $sqlFechaEstadoCuenta and cc.cod_unidadorganizacional in ($StringUnidades) and e.cod_cuentaaux in ($cod_cuentaauxX) and d.cod_unidadorganizacional in ($unidadCostoArray) and d.cod_area in ($areaCostoArray) GROUP BY e.cod_cuentaaux  order by e.fecha";
+    $stmt_d = $dbh->prepare($sql);
+    $stmt_d->execute();
+    $monto_ecD=0;
+    while ($row_d = $stmt_d->fetch()) {
+        $monto_ecD=$row_d['monto'];
+    }        
+    $saldo_X=$monto_ecX-$monto_ecD;
+    $periodo=0;
+    $fechai=$desde;
+    $i=0;
+    foreach ($array_periodo as $periodo) {
+        $fechaf=date('Y-m-d',strtotime($fechai.'+'.$periodo.' day'));
+        if($fechai<=$fechaDet and $fechaDet<=$fechaf){
+            $monto_periodo[$i]+=$monto_ecX;
         }
-        if($tipoDebeHaberX==2){//proveedor
-            // $totalCredito=$totalCredito+$monto_ecX;
-            // $totalDebito=$totalDebito+$monto_ecD;
-            // $saldo_X=$monto_ecX-$monto_ecD;
-            // $html.='<tr class="bg-white" >
-            //     <td class="text-center small">'.$index.'</td>
-            //     <td class="text-left small">'.$nombreX.'</td>
-            //     <td class="text-right small">'.formatNumberDec($saldo_X).'</td>
-            // </tr>'; 
-        }else{//cliente
-            $totalCredito=$totalCredito+$monto_ecD;
-            $totalDebito=$totalDebito+$monto_ecX;
-            $saldo_X=$monto_ecX-$monto_ecD;
-
-            $html.='<td class="text-right small">'.formatNumberDec($saldo_X).'</td>';
-        }
-        $index++;
+        $i++;
+        $fechai=$fechaf;        
     }
-
+    if($fechaDet>$fechaf){//si es mayor a 90 dias
+        $monto_periodo[$i]+=$monto_ecX;
+    }
+}
+$j=0;
+foreach ($array_periodo as $periodo) {    
+    echo '<td class="text-right small">'.formatNumberDec($monto_periodo[$j]).'</td>';
+    $j++;    
+}    
+echo '<td class="text-right small">'.formatNumberDec($monto_periodo[$j]).'</td>';
 
 }
-$html.='<td class="text-right small">'.formatNumberDec($saldo_X).'</td>';
 
 ?>

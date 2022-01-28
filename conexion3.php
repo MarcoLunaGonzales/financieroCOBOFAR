9ace75c7be90754fc756b6d4aef83f5647348cd1
= <=>
2022-01-28 17:40:10 -0400

    cambios en conexiones

--- conexion3.php
+++ conexion3.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion3 extends PDO { 
   private $tipo_de_base = 'mysql';
-  private $host = 'localhost';
-  private $nombre_de_base = 'financiero_cobofar';   
-  private $usuario = 'root';
-  private $contrasena = '';
+  private $host = '10.10.1.19';
+  private $nombre_de_base = 'financiero_cobofar_200';   
+  private $usuario = 'Cobofar77';
+  private $contrasena = 'B0l1v14.@1202**';
   private $port = '3306';
   
 
